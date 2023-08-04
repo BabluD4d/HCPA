600c52e44c74c39aaa36f5217fc0353148a4f256
sandeepd4d <sandeep.d4d@gmail.com>
2023-08-04 16:18:32 +0530

    upadte the base url

--- src/Api/BaseApi.js
+++ src/Api/BaseApi.js
@@ -1,7 +1,7 @@
 import { create } from 'apisauce';
 const BaseApi = create({
-      // baseURL: 'http://192.168.0.133:8008/api/',
-      baseURL: 'https://healthcare.digital4design.in/api/',
+  // baseURL: 'https://healthcare.digital4design.in/api/',
+    baseURL: 'http://16.171.100.244/api/',
     headers: {Authorization:'Bearer '+ localStorage.getItem("Token")},
 });
   // const BaseUrlImage = 'http://192.168.0.133:8008/'
